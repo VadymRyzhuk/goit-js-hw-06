0a5ca8466be892ee615db77dd07594dd96737cdc
VadymRyzhuk <ceasarj571@gmail.com>
2023-08-29 16:03:14 +0200

    done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+
+const button = document.querySelector('.change-color');
+const body = document.querySelector('body');
+const color = document.querySelector('.color');
+
+button.addEventListener('click', onClickButton);
+
+function onClickButton() {
+  body.style.backgroundColor = color.textContent = getRandomHexColor();
+};
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
